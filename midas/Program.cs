62b7050f5df971ad4e234e65a0eec54bb220d5f8
Oleg Kleiman <oleg_kleyman@hotmail.com>
2025-11-19 16:25:02 +0200

    Add project files.

---
+++ midas/Program.cs
@@ -0,0 +1,44 @@
+
+namespace midas
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddAuthorization();
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+            var summaries = new[]
+            {
+                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            };
+
+            app.MapGet("/otp", (string id, string phoneNum, HttpContext httpContext) =>
+            {
+
+                return forecast;
+            })
+            .WithName("RequestOTP")
+            .WithOpenApi();
+
+            app.Run();
+        }
+    }
+}
